a8ebb60441ed8133895f486cb64d17aea2446fd2
Barry Watson <128091822+BarryWatson@users.noreply.github.com>
2023-04-21 23:51:27 +0200

    Added variable radio transmit power

--- main.ts
+++ main.ts
@@ -1,6 +1,18 @@
+input.onButtonPressed(Button.A, function () {
+    power += -1
+    radio.setTransmitPower(power)
+    basic.showNumber(power)
+})
+input.onButtonPressed(Button.B, function () {
+    power += 1
+    radio.setTransmitPower(power)
+    basic.showNumber(power)
+})
+let power = 0
+power = 6
 radio.setGroup(1)
-radio.setTransmitPower(6)
+radio.setTransmitPower(power)
 basic.forever(function () {
     radio.sendNumber(0)
-    basic.pause(500)
+    basic.pause(1000)
 })
